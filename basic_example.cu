600fa33d8d2bf912f7c4ef64f2c1506280aae3d5
JoaoScheleder <jg.scheleder@hotmail.com>
2025-09-12 12:44:42 -0300

    Add basic CUDA example with kernel and CPU-GPU interaction
    - Implemented a simple CUDA kernel that prints a message from each GPU thread.
    - Added a main function to launch the kernel and synchronize the device.
    - Included necessary headers for CUDA runtime and standard I/O.

--- basic_example.cu
+++ basic_example.cu
@@ -3,12 +3,13 @@
 
 
 __global__ void myKernel(void) {
-    printf("Hello From GPU \n");
+    printf("Hello from GPU thread %d!\n", threadIdx.x);
 }   
 
 int main () {
     // blocks, threads (max threads per block = 1024)
     myKernel<<<1,10>>>();
+    cudaDeviceSynchronize();
     printf("Hello from CPU \n");
     return 0;
 }
